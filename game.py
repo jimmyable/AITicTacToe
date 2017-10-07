c849f7b7d3e340db155d8f64721c0f5ac4329638
Gorrila Tape <jimmyjoseph@outlook.com>
2017-10-07 22:41:50 +0100

    added moves etc

--- game.py
+++ game.py
@@ -18,4 +18,33 @@ def drawBoard(board):
     print('  ' + board[1] + '  |  ' + board[2] + '  |  ' + board[3])
     print('     |     |')
 
+def PlayerLetter():
+    #Let the player type which letter they want to be
+    #Returns a list with the players letter as the first item and the computer as second
+    letter = ""
+    while not (letter == 'X' or letter == 'O'):
+        print('Are you an X or an O?')
+        letter = input().upper()
+        if letter == 'X':
+            return ['X', 'O']
+        else:
+            return ['O', 'X']
+
+def FirstPlayer():
+    #randomly chooses the 1st PlayerLetter
+    if random.randint(0, 1) == 0:
+        return 'AI'
+    else:
+        return 'You'
+
+def playAgain():
+    #This function returns True if the player wants to play again
+    print('One more game! (Y or N)')
+    return input().upper().startswith('Y')
+
+def makeMove(board, letter, move):
+    board[move] = letter
+
+
+
 start=drawBoard(['X', 'X', 'X', 'X', 'X', 'O', 'X', 'X', 'X', 'O'])
